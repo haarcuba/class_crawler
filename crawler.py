1a5d4080c49fa7f703ec20dbc03384aa4f5c6839
Yoav Kleinberger <haarcuba@gmail.com>
2023-11-27 16:09:38 +0200

    code review

--- crawler.py
+++ crawler.py
@@ -2,10 +2,11 @@
 
 from html.parser import HTMLParser
 import argparse
-from yaml import dump
+import yaml
 from re import search
 from urllib.request import urlopen, Request
 from urllib.parse import urljoin
+import sys
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -21,12 +22,19 @@ class MyHTMLParser(HTMLParser):
         self.links = []
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'a':
-            for attr in attrs:
-                if attr[0] == 'href':
-                    if not search(arguments.ignore_regex, attr[1]):
-                        if not attr[1] in self.links:
-                            self.links.append(attr[1])
+        if tag != 'a':
+            return
+
+        attributes = attrs
+        for name, value in attributes:
+            if name != 'href':
+                continue
+
+            if search(arguments.ignore_regex, value):
+                continue
+
+            if not value in self.links:
+                self.links.append(value)
 
 
 def scan_urls(url):
@@ -34,14 +42,14 @@ def scan_urls(url):
     HTMLparser.feed(str(urlopen(url).read()))
     return [urljoin(url, link) for link in HTMLparser.links]
 
-def get_yaml():
-    temp = [{"level":i, "links":[]} for i in range(arguments.depth+1)]
-    dictionary = {"root":arguments.url, "web":temp}
-    dictionary["web"][0]["links"] = [arguments.url]
+def get_web_of_links():
+    levels = [{"level":i, "links":[]} for i in range(arguments.depth+1)]
+    result = {"root":arguments.url, "web":levels}
+    result["web"][0]["links"] = [arguments.url]
     for i in range(arguments.depth):
-        for link in dictionary["web"][i]["links"]:
-            dictionary["web"][i+1]["links"] += scan_urls(link)
-    return dump(dictionary)
+        for link in result["web"][i]["links"]:
+            result["web"][i+1]["links"] += scan_urls(link)
+    return result
 
 if __name__ == "__main__":
-    print(get_yaml())
+    yaml.dump(get_web_of_links(), stream=sys.stdout)
