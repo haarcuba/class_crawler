7a8d316015919724c9e9d821e1579dcc400fe62d
Yoav Kleinberger <haarcuba@gmail.com>
2023-11-27 17:22:29 +0200

    code that passes the links test

--- crawler/links.py
+++ crawler/links.py
@@ -1,6 +1,25 @@
+import html.parser
+
+
+class _LinksParser(html.parser.HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.links = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag != "a":
+            return
+
+        for name, value in attrs:
+            if name != 'href':
+                continue
+
+            self.links.append(value)
+
 class Links:
     def __init__(self, html):
-        pass
+        self._parser = _LinksParser()
+        self._parser.feed(html)
 
     def all(self):
-        pass
+        return self._parser.links
