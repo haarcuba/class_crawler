379365fede5241fff7ff58781c5f757a99d9383a
Yoav Kleinberger <haarcuba@gmail.com>
2023-12-04 17:45:08 +0200

    passing test for depth 4 tree

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -1,24 +1,33 @@
+import collections
+
 class Crawl:
     def __init__(self, root_url, *, find_links):
         self._find_links = find_links
         self._seen = set()
+        self._result = collections.defaultdict(list)
         self._go(root_url)
 
     def _go(self, root_url):
-        self._result = [[root_url]]
-        self._to_explore = [root_url]
+        self._result[0] = [root_url]
+        self._to_explore = [(0, root_url)]
         while len(self._to_explore) > 0:
-            url = self._to_explore.pop(0)
-            self._add_links_from(url)
+            depth, url = self._to_explore.pop(0)
+            self._add_links_from(depth, url)
 
-    def _add_links_from(self, url):
+    def _add_links_from(self, depth, url):
         links = self._find_links(url)
         self._seen.add(url)
         links = [link for link in links if link not in self._seen]
         if len(links) == 0:
             return
-        self._result.append(links)
-        self._to_explore += links
+
+        self._result[depth + 1] += links
+        self._to_explore += [(depth + 1, link) for link in links]
 
     def web_of_links(self):
-        return self._result
+        result = []
+        for depth in range(len(self._result)):
+            links = self._result[depth]
+            result.append(links)
+
+        return result
--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -33,3 +33,21 @@ def test_edge_case__depth_2_loop():
                 ['root'],
                 ['link1', 'link2'],
            ]
+
+def test_depth_4_link_tree():
+    with Scenario() as s:
+        s.find_links('root') >> ['link1', 'link2']
+        s.find_links('link1') >> ['link1.1', 'link1.2']
+        s.find_links('link2') >> ['link2.1']
+        s.find_links('link1.1') >> []
+        s.find_links('link1.2') >> ['link1.2.1']
+        s.find_links('link2.1') >> []
+        s.find_links('link1.2.1') >> []
+
+        tested = crawler.crawl.Crawl('root', find_links=Fake('find_links'))
+        assert tested.web_of_links() == [
+                ['root'],
+                ['link1', 'link2'],
+                ['link1.1', 'link1.2', 'link2.1'],
+                ['link1.2.1',]
+           ]
