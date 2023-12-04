11f2a4d4d3fa260aa5cc457bc69d14043f64664d
Yoav Kleinberger <haarcuba@gmail.com>
2023-12-04 15:56:55 +0200

    failing test for depth 2 tree

--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -9,3 +9,15 @@ def test_no_links__return_only_root_url():
         assert tested.web_of_links() == [
                 ["http://localhost:9999"],
            ]
+
+def test_depth_2_link_tree():
+    with Scenario() as s:
+        s.find_links("http://localhost:9999") >> ['link1', 'link2']
+        s.find_links("link1") >> []
+        s.find_links("link2") >> []
+
+        tested = crawler.crawl.Crawl("http://localhost:9999", find_links=Fake('find_links'))
+        assert tested.web_of_links() == [
+                ["http://localhost:9999"],
+                ["link1", "link2"],
+           ]
