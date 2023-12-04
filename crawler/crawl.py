bad031a4a3d6077e434ee57f08c8a73f7e280d84
Yoav Kleinberger <haarcuba@gmail.com>
2023-12-04 17:02:36 +0200

    refactoring

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -1,19 +1,21 @@
 class Crawl:
     def __init__(self, root_url, *, find_links):
-        self._root_url = root_url
+        self._find_links = find_links
+        self._go(root_url)
 
-        result = [[root_url]]
-        to_visit = [root_url]
-        while len(to_visit) > 0:
-            url = to_visit.pop(0)
-            links = find_links(url)
-            if len(links) == 0:
-                continue
-            to_visit += links
-            result.append(links)
+    def _go(self, root_url):
+        self._result = [[root_url]]
+        self._to_explore = [root_url]
+        while len(self._to_explore) > 0:
+            url = self._to_explore.pop(0)
+            self._add_links_from(url)
 
-        self._result = result
-
+    def _add_links_from(self, url):
+        links = self._find_links(url)
+        if len(links) == 0:
+            return
+        self._result.append(links)
+        self._to_explore += links
 
     def web_of_links(self):
         return self._result
