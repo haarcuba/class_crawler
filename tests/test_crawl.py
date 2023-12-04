3514a7a5ddea21db4121206a251389b356b7b9eb
Yoav Kleinberger <haarcuba@gmail.com>
2023-12-04 17:23:28 +0200

    you are not good enough to refactor tests

--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -3,22 +3,22 @@ from testix import *
 
 def test_no_links__return_only_root_url():
     with Scenario() as s:
-        s.find_links('http://localhost:9999') >> []
+        s.find_links('root') >> []
 
-        tested = crawler.crawl.Crawl('http://localhost:9999', find_links=Fake('find_links'))
+        tested = crawler.crawl.Crawl('root', find_links=Fake('find_links'))
         assert tested.web_of_links() == [
-                ['http://localhost:9999'],
+                ['root'],
            ]
 
 def test_depth_2_link_tree():
     with Scenario() as s:
-        s.find_links('http://localhost:9999') >> ['link1', 'link2']
+        s.find_links('root') >> ['link1', 'link2']
         s.find_links('link1') >> []
         s.find_links('link2') >> []
 
-        tested = crawler.crawl.Crawl('http://localhost:9999', find_links=Fake('find_links'))
+        tested = crawler.crawl.Crawl('root', find_links=Fake('find_links'))
         assert tested.web_of_links() == [
-                ['http://localhost:9999'],
+                ['root'],
                 ['link1', 'link2'],
            ]
 
