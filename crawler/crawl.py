9eaa4b75fc9a9988e038d814a7f8ee28732596ce
Yoav Kleinberger <haarcuba@gmail.com>
2023-12-04 15:50:30 +0200

    passing test for no links

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -1,6 +1,7 @@
 class Crawl:
-    def __init__(self, root_url):
+    def __init__(self, root_url, *, find_links):
         self._root_url = root_url
+        find_links(root_url)
 
     def web_of_links(self):
         return [ [self._root_url] ]
--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -1,7 +1,11 @@
 import crawler.crawl
+from testix import *
 
 def test_no_links__return_only_root_url():
-    tested = crawler.crawl.Crawl("http://localhost:9999")
-    assert tested.web_of_links() == [
-            ["http://localhost:9999"],
-       ]
+    with Scenario() as s:
+        s.find_links("http://localhost:9999") >> []
+
+        tested = crawler.crawl.Crawl("http://localhost:9999", find_links=Fake('find_links'))
+        assert tested.web_of_links() == [
+                ["http://localhost:9999"],
+           ]
