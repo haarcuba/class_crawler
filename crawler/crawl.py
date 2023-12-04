6f417938c1f3849b3bbc795b577deb821aaf5142
Yoav Kleinberger <haarcuba@gmail.com>
2023-12-04 16:06:31 +0200

    passing test for simple 2 depth tree

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -1,7 +1,19 @@
 class Crawl:
     def __init__(self, root_url, *, find_links):
         self._root_url = root_url
-        find_links(root_url)
+
+        result = [[root_url]]
+        to_visit = [root_url]
+        while len(to_visit) > 0:
+            url = to_visit.pop(0)
+            links = find_links(url)
+            if len(links) == 0:
+                continue
+            to_visit += links
+            result.append(links)
+
+        self._result = result
+
 
     def web_of_links(self):
-        return [ [self._root_url] ]
+        return self._result
