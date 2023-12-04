4a4a59fca37e64803ec2e1c52f6d0f3c866a769b
Yoav Kleinberger <haarcuba@gmail.com>
2023-12-04 17:17:43 +0200

    pass test for loop edge case

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -1,6 +1,7 @@
 class Crawl:
     def __init__(self, root_url, *, find_links):
         self._find_links = find_links
+        self._seen = set()
         self._go(root_url)
 
     def _go(self, root_url):
@@ -12,6 +13,8 @@ class Crawl:
 
     def _add_links_from(self, url):
         links = self._find_links(url)
+        self._seen.add(url)
+        links = [link for link in links if link not in self._seen]
         if len(links) == 0:
             return
         self._result.append(links)
