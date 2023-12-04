745f3fde8ae27f53b0373e4a55aee1c93aab1e51
Yoav Kleinberger <haarcuba@gmail.com>
2023-12-04 17:06:55 +0200

    replace all double quotes with single quotes

--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -3,21 +3,21 @@ from testix import *
 
 def test_no_links__return_only_root_url():
     with Scenario() as s:
-        s.find_links("http://localhost:9999") >> []
+        s.find_links('http://localhost:9999') >> []
 
-        tested = crawler.crawl.Crawl("http://localhost:9999", find_links=Fake('find_links'))
+        tested = crawler.crawl.Crawl('http://localhost:9999', find_links=Fake('find_links'))
         assert tested.web_of_links() == [
-                ["http://localhost:9999"],
+                ['http://localhost:9999'],
            ]
 
 def test_depth_2_link_tree():
     with Scenario() as s:
-        s.find_links("http://localhost:9999") >> ['link1', 'link2']
-        s.find_links("link1") >> []
-        s.find_links("link2") >> []
+        s.find_links('http://localhost:9999') >> ['link1', 'link2']
+        s.find_links('link1') >> []
+        s.find_links('link2') >> []
 
-        tested = crawler.crawl.Crawl("http://localhost:9999", find_links=Fake('find_links'))
+        tested = crawler.crawl.Crawl('http://localhost:9999', find_links=Fake('find_links'))
         assert tested.web_of_links() == [
-                ["http://localhost:9999"],
-                ["link1", "link2"],
+                ['http://localhost:9999'],
+                ['link1', 'link2'],
            ]
