1bd195a59d408cf3a75c0945a37a20de8b5193aa
Yoav Kleinberger <haarcuba@gmail.com>
2023-12-27 18:31:22 +0200

    consistent naming

--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -3,20 +3,20 @@ from testix import *
 
 def test_no_links__return_only_root_url():
     with Scenario() as s:
-        s.find_links('root') >> []
+        s.find_urls('root') >> []
 
-        tested = crawler.crawl.Crawl('root', find_urls=Fake('find_links'))
+        tested = crawler.crawl.Crawl('root', find_urls=Fake('find_urls'))
         assert tested.web_of_links() == [
                 ['root'],
            ]
 
 def test_depth_2_link_tree():
     with Scenario() as s:
-        s.find_links('root') >> ['link1', 'link2']
-        s.find_links('link1') >> []
-        s.find_links('link2') >> []
+        s.find_urls('root') >> ['link1', 'link2']
+        s.find_urls('link1') >> []
+        s.find_urls('link2') >> []
 
-        tested = crawler.crawl.Crawl('root', find_urls=Fake('find_links'))
+        tested = crawler.crawl.Crawl('root', find_urls=Fake('find_urls'))
         assert tested.web_of_links() == [
                 ['root'],
                 ['link1', 'link2'],
@@ -24,11 +24,11 @@ def test_depth_2_link_tree():
 
 def test_edge_case__depth_2_loop():
     with Scenario() as s:
-        s.find_links('root') >> ['link1', 'link2']
-        s.find_links('link1') >> []
-        s.find_links('link2') >> ['root']
+        s.find_urls('root') >> ['link1', 'link2']
+        s.find_urls('link1') >> []
+        s.find_urls('link2') >> ['root']
 
-        tested = crawler.crawl.Crawl('root', find_urls=Fake('find_links'))
+        tested = crawler.crawl.Crawl('root', find_urls=Fake('find_urls'))
         assert tested.web_of_links() == [
                 ['root'],
                 ['link1', 'link2'],
@@ -36,15 +36,15 @@ def test_edge_case__depth_2_loop():
 
 def test_depth_4_link_tree():
     with Scenario() as s:
-        s.find_links('root') >> ['link1', 'link2']
-        s.find_links('link1') >> ['link1.1', 'link1.2']
-        s.find_links('link2') >> ['link2.1']
-        s.find_links('link1.1') >> []
-        s.find_links('link1.2') >> ['link1.2.1']
-        s.find_links('link2.1') >> []
-        s.find_links('link1.2.1') >> []
+        s.find_urls('root') >> ['link1', 'link2']
+        s.find_urls('link1') >> ['link1.1', 'link1.2']
+        s.find_urls('link2') >> ['link2.1']
+        s.find_urls('link1.1') >> []
+        s.find_urls('link1.2') >> ['link1.2.1']
+        s.find_urls('link2.1') >> []
+        s.find_urls('link1.2.1') >> []
 
-        tested = crawler.crawl.Crawl('root', find_urls=Fake('find_links'))
+        tested = crawler.crawl.Crawl('root', find_urls=Fake('find_urls'))
         assert tested.web_of_links() == [
                 ['root'],
                 ['link1', 'link2'],
