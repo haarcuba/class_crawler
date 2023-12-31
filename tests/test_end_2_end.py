9851452562d71fb89bbc00b633161ec6bce21a5b
Anat <anatp2417@gmail.com>
2023-12-31 13:35:15 +0200

    failing e2e test

--- tests/test_end_2_end.py
+++ tests/test_end_2_end.py
@@ -1,5 +1,9 @@
 import pytest
+from testix import *
 import subprocess
+import crawler.crawl as crawl
+import crawler.links as links
+
 
 @pytest.fixture
 def file_server():
@@ -10,5 +14,12 @@ def file_server():
 
 
 def test_end_2_end__crawler_actually_works(file_server):
-    print()
-    print(f'got {file_server=}')
+    with Scenario() as s:
+        tested = crawl.Crawl(file_server, depth=100, find_urls=links.Links)
+        assert tested.web_of_links() == [
+            [f'{file_server}'],
+            [f'{file_server}/a', f'{file_server}/d', f'{file_server}/myfile.html'],
+            [f'{file_server}/a/b', f'{file_server}/a/myfile.html', f'{file_server}/d/myfile.html'],
+            [f'{file_server}/a/b/c', f'{file_server}/a/b/myfile.html'],
+            [f'{file_server}/a/b/c/c.html'],
+        ]
