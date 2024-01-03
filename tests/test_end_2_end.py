2736d43fcad715565b97697616662920ab94bdee
Anat <paliy@mail.tau.ac.il>
2024-01-03 17:03:30 +0200

    changed e2e test to use sets

--- tests/test_end_2_end.py
+++ tests/test_end_2_end.py
@@ -16,10 +16,10 @@ def file_server():
 def test_end_2_end__crawler_actually_works(file_server):
     with Scenario() as s:
         tested = crawl.Crawl(file_server, depth=100, find_urls=links.Links)
-        assert tested.web_of_links() == [
-            [f'{file_server}'],
-            [f'{file_server}/a', f'{file_server}/d', f'{file_server}/myfile.html'],
-            [f'{file_server}/a/b', f'{file_server}/a/myfile.html', f'{file_server}/d/myfile.html'],
-            [f'{file_server}/a/b/c', f'{file_server}/a/b/myfile.html'],
-            [f'{file_server}/a/b/c/c.html'],
+        assert list(map(set, tested.web_of_links())) == [
+            {f'{file_server}'},
+            {f'{file_server}/a/', f'{file_server}/d/', f'{file_server}/myfile.html'},
+            {f'{file_server}/a/b/', f'{file_server}/a/myfile.html', f'{file_server}/d/myfile.html'},
+            {f'{file_server}/a/b/c/', f'{file_server}/a/b/myfile.html'},
+            {f'{file_server}/a/b/c/c.html'},
         ]
