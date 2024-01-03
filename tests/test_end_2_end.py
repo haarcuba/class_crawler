e8f5343099069a05fd6c2d2dfe3353852087180f
paliy <paliy@mail.tau.ac.il>
2024-01-03 17:28:53 +0200

    changed e2e test according to code review

--- tests/test_end_2_end.py
+++ tests/test_end_2_end.py
@@ -1,8 +1,7 @@
 import pytest
-from testix import *
 import subprocess
-import crawler.crawl as crawl
-import crawler.links as links
+import crawler.crawl
+import crawler.links
 
 
 @pytest.fixture
@@ -14,12 +13,11 @@ def file_server():
 
 
 def test_end_2_end__crawler_actually_works(file_server):
-    with Scenario() as s:
-        tested = crawl.Crawl(file_server, depth=100, find_urls=links.Links)
-        assert list(map(set, tested.web_of_links())) == [
-            {f'{file_server}'},
-            {f'{file_server}/a/', f'{file_server}/d/', f'{file_server}/myfile.html'},
-            {f'{file_server}/a/b/', f'{file_server}/a/myfile.html', f'{file_server}/d/myfile.html'},
-            {f'{file_server}/a/b/c/', f'{file_server}/a/b/myfile.html'},
-            {f'{file_server}/a/b/c/c.html'},
-        ]
+    tested = crawler.crawl.Crawl(file_server, depth=100, find_urls=crawler.links.Links)
+    assert list(map(set, tested.web_of_links())) == [
+        {f'{file_server}'},
+        {f'{file_server}/a/', f'{file_server}/d/', f'{file_server}/myfile.html'},
+        {f'{file_server}/a/b/', f'{file_server}/a/myfile.html', f'{file_server}/d/myfile.html'},
+        {f'{file_server}/a/b/c/', f'{file_server}/a/b/myfile.html'},
+        {f'{file_server}/a/b/c/c.html'},
+    ]
