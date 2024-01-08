fb6446f20c55e710d1e2d70bcca721964f4c71c2
paliy <paliy@mail.tau.ac.il>
2024-01-08 17:27:10 +0200

    find_urls.FindUrls now works

--- crawler/find_urls.py
+++ crawler/find_urls.py
@@ -1,3 +1,20 @@
+import urllib.request
+import urllib.parse
+import links
+
+
+def _url_parser(url):
+    def url_parse(link):
+        return urllib.parse.urljoin(url, link)
+    return url_parse
+
+
 class FindUrls:
+    def __init__(self, parser=links.Links):
+        self.parser = parser
+
     def __call__(self, url):
-        return []
+        html = urllib.request.urlopen(url).read()
+        parsed = self.parser(str(html))
+        links = parsed.all()
+        return map(_url_parser(url), links)
